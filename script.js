08af33ea557aae4e79704a7d544fc5059f5c6f3a
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-09-14 20:29:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+const para=document.getElementsByTagName('p')[0];
+const image=document.querySelector('img');
+const buttons=document.querySelectorAll('button');
+buttons.forEach(button=>button.addEventListener('click',(event)=>{
+
+  if()
+	
+}))
+async function displayData(){
+
+	let response=await fetch('https://randomuser.me/api/');
+	let data=await response.json();
+	// image.src=data.results[0].picture.large;
+
+	
+	
+}
+
+//displayData();
